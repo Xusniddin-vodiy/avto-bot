b35e3e8327f2298c7256cc9ff11582367c88362e
Xusniddin-vodiy <husniddinhakimov9596@gmail.com>
2026-04-28 22:41:35 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,51 @@
 import os
-import telebot
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
 
-TOKEN = os.getenv("BOT_TOKEN")
+API_TOKEN = os.getenv("8668632754:AAEXGwJZB4pVuEdPgjELvJiB1PBlBoc9K98")
 
-bot = telebot.TeleBot(TOKEN)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Salom! Bot ishlayapti 🚀")
+channels = {
+    "#tahoe": -1003904657707,
+    "#equinox": -1003539247125,
+    "#malibu": -1003752675712,
+    "#onix": -1003973192547,
+    "#captiva": -1003827894357,
+    "#nexia": -1003975266175,
+    "#gentra": -1003989860274,
+    "#matiz": -1003878990621,
+    "#spark": -1003738707897,
+    "#damas": -1003809354458,
+    "#labo": -1003809354458,
+    "#monza": -1003797749107,
+    "#tracker": -1003972713851,
+    "#orlando": -1003850462605,
+    "#trailblazer": -1003806863783,
+    "#tico": -1003877691183,
+    "#fura": -1003993025292,
+    "#inomarka": -1003666897042,
+    "#cobalt": -1001484563003,
+}
 
-bot.infinity_polling()
+# 🔴 ASOSIY KANAL (shu eng muhim!)
+SOURCE_CHANNEL = -1001437422385
+
+@dp.channel_post_handler()
+async def repost(message: types.Message):
+    text = (message.text or message.caption or "").lower()
+
+    if message.chat.id != SOURCE_CHANNEL:
+        return
+
+    for tag, channel_id in channels.items():
+        if tag in text:
+            await bot.copy_message(
+                chat_id=channel_id,
+                from_chat_id=message.chat.id,
+                message_id=message.message_id
+            )
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
